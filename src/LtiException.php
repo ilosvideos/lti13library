8c09b0ee134393a9d7a4c0cc6662535ce03aaf9f
ryan-schroeder <ryan.schroeder@vidgrid.com>
2022-11-10 18:30:30 -0600

    add custom details field to LtiException class

--- src/LtiException.php
+++ src/LtiException.php
@@ -3,5 +3,15 @@ namespace IMSGlobal\LTI;
 
 class LtiException extends \Exception {
 
+    public $details;
+
+    public function __construct($message = '', $code = 0, Throwable $previous = null, $details = null) {
+        $this->details = $details;
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function getDetails() {
+        return $this->details;
+    }
 }
 ?>
